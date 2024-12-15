73028e2b26255451a2fd4c789d3cd230839f3ef1
GyMatyi <gyimesimatyas@gmail.com>
2024-12-15 17:54:40 +0100

    finishpoint teszt

--- Assets/Scripts/Tests/PlayMode/FinishPoint1SpawnerGOTests.cs
+++ Assets/Scripts/Tests/PlayMode/FinishPoint1SpawnerGOTests.cs
@@ -1,69 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
-using System.Collections;
-
 
 public class FinishPoint1SpawnerGOTests
 {
-    private GameObject spawnerObject;
-    private FinishPoint1SpawnerGO spawnerScript;
-    private GameObject finishPointPrefab;
-
-    [SetUp]
-    public void SetUp()
+    // A Test behaves as an ordinary method
+    [Test]
+    public void FinishPoint1SpawnerGOTestsSimplePasses()
     {
-        // Létrehozzuk a Finish Point prefab-et
-        finishPointPrefab = new GameObject("FinishPoint1GO");
-        finishPointPrefab.tag = "FinishPoint1Tag";
-
-        // Létrehozzuk a spawner objektumot
-        spawnerObject = new GameObject("FinishPoint1SpawnerGO");
-        spawnerScript = spawnerObject.AddComponent<FinishPoint1SpawnerGO>();
-        spawnerScript.FinishPoint1GO = finishPointPrefab;
+        // Use the Assert class to test conditions
     }
 
-    [TearDown]
-    public void TearDown()
-    {
-        Object.DestroyImmediate(finishPointPrefab);
-        Object.DestroyImmediate(spawnerObject);
-    }
-
+    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+    // `yield return null;` to skip a frame.
     [UnityTest]
-    public IEnumerator SpawnFinishPoint1_SpawnsFinishPointOnEvent()
+    public IEnumerator FinishPoint1SpawnerGOTestsWithEnumeratorPasses()
     {
-        // Ellenõrizzük, hogy még nincs Finish Point a jelenetben
-        Assert.IsNull(GameObject.FindWithTag("FinishPoint1Tag"), "Finish Point should not exist before the event is triggered.");
-
-        // Meghívjuk az OnBoss1Destroyed eseményt
-        object value = Boss1Controller.OnBoss1Destroyed?.Invoke();
-
-        // Várjunk egy frame-et, hogy az esemény kezelése megtörténjen
+        // Use the Assert class to test conditions.
+        // Use yield to skip a frame.
         yield return null;
-
-        // Ellenõrizzük, hogy a Finish Point prefab példányosítva lett
-        var spawnedFinishPoint = GameObject.FindWithTag("FinishPoint1Tag");
-        Assert.IsNotNull(spawnedFinishPoint, "Finish Point should be spawned after the event is triggered.");
-        Assert.AreEqual(Vector3.zero, spawnedFinishPoint.transform.position, "Finish Point should be spawned at position (0, 0, 0).");
-    }
-
-    [UnityTest]
-    public IEnumerator DestroyActiveFinishPoint1_DestroysFinishPoint()
-    {
-        // Létrehozzuk manuálisan a Finish Point objektumot
-        var spawnedFinishPoint = Object.Instantiate(finishPointPrefab, Vector3.zero, Quaternion.identity);
-
-        // Ellenõrizzük, hogy a Finish Point létezik
-        Assert.IsNotNull(GameObject.FindWithTag("FinishPoint1Tag"), "Finish Point should exist before calling DestroyActiveFinishPoint1.");
-
-        // Meghívjuk az DestroyActiveFinishPoint1 metódust
-        spawnerScript.DestroyActiveFinishPoint1();
-
-        // Várjunk egy frame-et, hogy az objektum törlése megtörténjen
-        yield return null;
-
-        // Ellenõrizzük, hogy a Finish Point már nem létezik
-        Assert.IsNull(GameObject.FindWithTag("FinishPoint1Tag"), "Finish Point should be destroyed after calling DestroyActiveFinishPoint1.");
     }
 }
