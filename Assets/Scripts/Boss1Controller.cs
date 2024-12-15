eba502ece34a757e30b18f9b4a35050a376b1e2c
GyMatyi <gyimesimatyas@gmail.com>
2024-12-15 18:25:30 +0100

    finishpointfasz

--- Assets/Scripts/Boss1Controller.cs
+++ Assets/Scripts/Boss1Controller.cs
@@ -3,21 +3,24 @@ using UnityEngine;
 
 public class Boss1Controller : MonoBehaviour
 {
-    public float speed = 0f;
-    public int Lives = 12;
-    public GameObject bulletPrefab;
-    public Transform bulletSpawnPoint;
-    public Transform player;
-    private GameObject scoreUITextGO;
-    private bool movingRight = true;
-    public GameObject ExplosionGO;
 
-    // Esemény deklaráció
+    public float speed = 0f;                   // Főgonosz sebessége
+    public int Lives = 12;                    // Boss1 élete
+    public GameObject bulletPrefab;           // Lövedék prefabja
+    public Transform bulletSpawnPoint;        // Lövedék kilövési pozíciója
+    public Transform player;                  // Játékos pozíciója
+    GameObject scoreUITextGO;                 // Pontszám UI
+    private bool movingRight = true;          // Kezdeti mozgásirány jobbra
+    public GameObject ExplosionGO;            //explosion prefab
+    public delegate void Boss1DestroyedEvent();
     public static event Action OnBoss1Destroyed;
 
     void Start()
     {
+        // Kezdetben véletlenszerűen állítja be az irányt
         movingRight = UnityEngine.Random.value > 0.5f;
+
+        // Megkeressük a pontszámkezelő UI-t 
         scoreUITextGO = GameObject.FindGameObjectWithTag("ScoreTextTag");
     }
 
@@ -26,16 +29,25 @@ public class Boss1Controller : MonoBehaviour
         Move();
     }
 
+    // Főgonosz jobbra-balra mozgatása
     void Move()
     {
-        float moveDirection = movingRight ? 1f : -1f;
-        transform.Translate(Vector2.right * moveDirection * speed * Time.deltaTime);
+        // Jobbra vagy balra mozdul el az aktuális irány alapján
+        if (movingRight)
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
 
-        if (transform.position.x >= 4.5f)
+        // Ha eléri a képernyő széleit, irányt vált
+        if (transform.position.x >= 4.5f) // Jobb széle (változtatható a képernyő szélesség szerint)
         {
             movingRight = false;
         }
-        else if (transform.position.x <= -4.5f)
+        else if (transform.position.x <= -4.5f) // Bal széle (változtatható)
         {
             movingRight = true;
         }
@@ -43,39 +55,43 @@ public class Boss1Controller : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D col)
     {
+        // Érzékeli, ha a játékos lövedékei vagy hajója eltalálja a boss1-et
         if (col.CompareTag("PlayerBulletTag01") || col.CompareTag("PlayerBulletTag02") || col.CompareTag("PlayerShipTag"))
         {
+            // Csökkentjük a boss életét
             Lives--;
 
             PlayExplosion();
 
+            // Növeljük a játékos pontszámát
             if (scoreUITextGO != null)
             {
                 scoreUITextGO.GetComponent<GameScore>().Score += 100;
             }
 
+            // Ha az élet 0, elpusztítjuk a boss1-et
             if (Lives <= 0)
             {
-                TriggerBoss1DestroyedEvent();
+                OnBoss1Destroyed?.Invoke(); // Esemény indítása
                 Destroy(gameObject);
                 scoreUITextGO.GetComponent<GameScore>().Score += 100;
             }
 
+            // Elpusztítjuk a játékos lövedékét (ha az találta el)
             if (col.CompareTag("PlayerBulletTag01") || col.CompareTag("PlayerBulletTag02"))
             {
                 Destroy(col.gameObject);
             }
         }
     }
-
+    //function to intantiate an explosion
     void PlayExplosion()
     {
-        GameObject explosion = Instantiate(ExplosionGO);
+        GameObject explosion = (GameObject)Instantiate(ExplosionGO);
+
+        //set the position of the explosion
         explosion.transform.position = transform.position;
     }
 
-    private void TriggerBoss1DestroyedEvent()
-    {
-        OnBoss1Destroyed?.Invoke(); // Esemény meghívása az osztályon belül
-    }
+
 }
--- Assets/Scripts/Tests/PlayMode/FinishPoint1SpawnerGOTests.cs
+++ Assets/Scripts/Tests/PlayMode/FinishPoint1SpawnerGOTests.cs
@@ -1,25 +1,69 @@
 using System.Collections;
-using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 
 public class FinishPoint1SpawnerGOTests
 {
-    // A Test behaves as an ordinary method
-    [Test]
-    public void FinishPoint1SpawnerGOTestsSimplePasses()
+    private GameObject spawnerObject;
+    private FinishPoint1SpawnerGO spawnerScript;
+    private GameObject finishPointPrefab;
+
+    [SetUp]
+    public void SetUp()
     {
-        // Use the Assert class to test conditions
+        // Létrehozzuk a Finish Point prefab-et
+        finishPointPrefab = new GameObject("FinishPoint1GO");
+        finishPointPrefab.tag = "FinishPoint1Tag";
+
+        // Létrehozzuk a spawner objektumot
+        spawnerObject = new GameObject("FinishPoint1SpawnerGO");
+        spawnerScript = spawnerObject.AddComponent<FinishPoint1SpawnerGO>();
+        spawnerScript.FinishPoint1GO = finishPointPrefab;
     }
 
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // `yield return null;` to skip a frame.
-    [UnityTest]
-    public IEnumerator FinishPoint1SpawnerGOTestsWithEnumeratorPasses()
+    [TearDown]
+    public void TearDown()
     {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
+        Object.DestroyImmediate(finishPointPrefab);
+        Object.DestroyImmediate(spawnerObject);
+    }
+
+    [UnityTest]
+    public IEnumerator SpawnFinishPoint1_SpawnsFinishPointOnEvent()
+    {
+        // Ellenõrizzük, hogy még nincs Finish Point a jelenetben
+        Assert.IsNull(GameObject.FindWithTag("FinishPoint1Tag"), "Finish Point should not exist before the event is triggered.");
+
+        // Meghívjuk az OnBoss1Destroyed eseményt
+        Boss1Controller.OnBoss1Destroyed?.Invoke();
+
+        // Várjunk egy frame-et, hogy az esemény kezelése megtörténjen
         yield return null;
+
+        // Ellenõrizzük, hogy a Finish Point prefab példányosítva lett
+        var spawnedFinishPoint = GameObject.FindWithTag("FinishPoint1Tag");
+        Assert.IsNotNull(spawnedFinishPoint, "Finish Point should be spawned after the event is triggered.");
+        Assert.AreEqual(Vector3.zero, spawnedFinishPoint.transform.position, "Finish Point should be spawned at position (0, 0, 0).");
+    }
+
+    [UnityTest]
+    public IEnumerator DestroyActiveFinishPoint1_DestroysFinishPoint()
+    {
+        // Létrehozzuk manuálisan a Finish Point objektumot
+        var spawnedFinishPoint = Object.Instantiate(finishPointPrefab, Vector3.zero, Quaternion.identity);
+
+        // Ellenõrizzük, hogy a Finish Point létezik
+        Assert.IsNotNull(GameObject.FindWithTag("FinishPoint1Tag"), "Finish Point should exist before calling DestroyActiveFinishPoint1.");
+
+        // Meghívjuk az DestroyActiveFinishPoint1 metódust
+        spawnerScript.DestroyActiveFinishPoint1();
+
+        // Várjunk egy frame-et, hogy az objektum törlése megtörténjen
+        yield return null;
+
+        // Ellenõrizzük, hogy a Finish Point már nem létezik
+        Assert.IsNull(GameObject.FindWithTag("FinishPoint1Tag"), "Finish Point should be destroyed after calling DestroyActiveFinishPoint1.");
     }
 }
+
